92612daa0b27805af9a45c1b9aa4713a68a8f0f4
saiteja-991999 <121968978+saiteja-991999@users.noreply.github.com>
2025-08-30 15:21:06 +0530

    Create MessageModal.js

---
+++ src/components/MessageModal.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+function MessageModal({ message, resetGame }) {
+  return (
+    <div className="modal-overlay" onClick={resetGame}>
+      <div className="modal" onClick={(e) => e.stopPropagation()}>
+        <h3>{message.title}</h3>
+        <p>{message.text}</p>
+        <div style={{ display: "flex", justifyContent: "center", marginTop: 12 }}>
+          <button className="btn ghost" onClick={resetGame}>Close</button>
+          <button className="btn primary" onClick={resetGame}>Play again</button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default MessageModal;
