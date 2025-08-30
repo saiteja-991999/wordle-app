6ae56ff3c0002f948ce439092f3d60311deb9b1d
saiteja-991999 <121968978+saiteja-991999@users.noreply.github.com>
2025-08-30 15:20:24 +0530

    Create Keyboard.js

---
+++ src/components/Keyboard.js
@@ -0,0 +1,29 @@
+import React from "react";
+
+function Keyboard({ handleKey }) {
+  const keyRows = [
+    ["Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P"],
+    ["A", "S", "D", "F", "G", "H", "J", "K", "L"],
+    ["ENTER", "Z", "X", "C", "V", "B", "N", "M", "DEL"]
+  ];
+
+  return (
+    <div className="keyboard">
+      {keyRows.map((row, ri) => (
+        <div key={ri} className="key-row">
+          {row.map((key) => (
+            <button
+              key={key}
+              onClick={() => handleKey(key)}
+              className={`key ${key === "ENTER" || key === "DEL" ? "wide" : ""}`}
+            >
+              {key}
+            </button>
+          ))}
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default Keyboard;
